163510029ea5134078f165d77d26de72f8910a9f
Alejo <alejandrofranco@pursuit.org>
2019-11-16 10:51:51 -0500

    refactor tests and add test for second user creation

--- tests/users.test.js
+++ tests/users.test.js
@@ -9,22 +9,49 @@ beforeAll(() => {
 
 describe('User functionality and routes', () => {
   it('[POST] to /users - Should add a new user', (done) => {
-    expect.assertions(3)
+    expect.assertions(4)
     request(app)
       .post('/users/signup')
       .send({ username: "JonSnow123" })
       .end((err, res) => {
         if (err) throw err
-        expect(res.status).toBe(201)
-
+        const statusCode = res.status
         const data = res.body
-        expect(data.payload).toEqual({
-          msg: "User created",
-          user: {
-            id: 1,
-            username: "JonSnow123"
-          }
-        })
+
+        expect(statusCode).toBe(201)
+        expect(data.payload.msg).toEqual("User created")
+        expect(data.payload.user.username).toEqual("JonSnow123")
+        expect(data.err).toBeFalsy()
+        done()
+      })
+  })
+
+  it('[POST] to /users with repeated username should return error', (done) => {
+    expect.assertions(2)
+    request(app)
+      .post('/users/signup')
+      .send({ username: "JonSnow123" })
+      .end((err, res) => {
+        if (err) throw err
+        expect(res.status).toBe(409)
+        expect(res.body.err).toBeTruthy()
+        done()
+      })
+  })
+
+  it('[POST] to /users - Adding a second new user should succed', (done) => {
+    expect.assertions(4)
+    request(app)
+      .post('/users/signup')
+      .send({ username: "AryaStark" })
+      .end((err, res) => {
+        if (err) throw err
+        const statusCode = res.status
+        const data = res.body
+
+        expect(statusCode).toBe(201)
+        expect(data.payload.msg).toEqual("User created")
+        expect(data.payload.user.username).toEqual("AryaStark")
         expect(data.err).toBeFalsy()
         done()
       })
