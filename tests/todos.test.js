680cd92172670bdd17c74fec5ca0f5bcd090d6b6
Alejo <alejandrofranco@pursuit.org>
2019-11-16 12:53:27 -0500

    [tests] add two tests to be done and explicitly state expected status code in test names

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -41,7 +41,7 @@ beforeAll(async () => {
 })
 
 describe('Todos functionality and routes', () => {
-  it('[POST] to /todos - Should add a new todo', (done) => {
+  it('[POST] to /todos - Should add a new todo and return status 200', (done) => {
     expect.assertions(5)
 
     const todoText = "1st Todo"
@@ -67,7 +67,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[POST] to /todos without a pre-registered owner should return error', (done) => {
+  it('[POST] to /todos without a pre-registered owner should return 400 status and error message', (done) => {
     expect.assertions(2)
 
     const todoText = "2nd Todo"
@@ -90,7 +90,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[POST] to /todos without a `owner` or `text` body data should return error', (done) => {
+  it('[POST] to /todos without a `owner` or `text` body data should return a 400 status and error message', (done) => {
     expect.assertions(2)
 
     request(app)
@@ -107,7 +107,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[GET] to /todos Retrieves all todos and returns them in an array', (done) => {
+  it('[GET] to /todos successfuly (status 200) retrieves all todos and returns them in an array.', (done) => {
     expect.assertions(4)
 
     request(app)
@@ -125,7 +125,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[PUT] to /todos/:id should fully update/replace a todo', async (done) => {
+  it('[PUT] to /todos/:id should fully update/replace a todo with status 200', async (done) => {
     expect.assertions(5)
 
     const addTodoToBeUpdated = async () => {
@@ -167,7 +167,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[PUT] to /todos/101 should fully create a new todo with id 101', async (done) => {
+  it('[PUT] to /todos/101 should fully create a new todo with id 101. Return a 201 status', async (done) => {
     expect.assertions(3)
 
     const todo = {
@@ -192,7 +192,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[PATCH] to /todos/101 should update text and set completed to true on todo 101', async (done) => {
+  it('[PATCH] to /todos/101 should update text and set completed to true on todo 101. Returns a 200 status', async (done) => {
     expect.assertions(4)
 
     const todo = {
@@ -216,7 +216,7 @@ describe('Todos functionality and routes', () => {
       })
   })
 
-  it('[DELETE] to /todos/101 should remove todo with id 101', async (done) => {
+  it('[DELETE] to /todos/101 should remove todo with id 101 and returns 200 status code', async (done) => {
     expect.assertions(5)
 
     const removedTodoProps = {
@@ -241,7 +241,7 @@ describe('Todos functionality and routes', () => {
   })
 
 
-  it('[GET] to /todos with `owner` query params retrieves all todos belonging to `owner`', (done) => {
+  it('[GET] to /todos with `owner` query params retrieves all todos belonging to `owner`. Returns 200 status code', (done) => {
     expect.assertions(4)
 
     request(app)
@@ -263,4 +263,6 @@ describe('Todos functionality and routes', () => {
   // TODO test deleting a non-existent todo
   // TODO test PUT with a non-existent owner
   // TODO fix put with completed true case mentioned above
+  // TODO test getting todos with query string `completed=true`
+  // TODO test getting with query string combination of `owner` & `completed`
 })
